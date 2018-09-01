b74d7883393871b2c8d33785ed146cb4004d6cb2
Alexander Sadovsky <alino.sadovsky@gmail.com>
2018-09-01 18:10:40 +0200

    removed accidental crap file

--- testmap.mm
+++
@@ -1,5 +0,0 @@
-<map version="1.0.1">
-<!-- This file was generated from a JSON structure, using json-to-freemind. -->
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node TEXT="test.json"><node TEXT="data"><node TEXT="user"><node TEXT="id"><node TEXT="type"><node TEXT="integer"/></node><node TEXT="incremental"><node TEXT="true"/></node></node><node TEXT="name"><node TEXT="type"><node TEXT="string"/></node><node TEXT="required"><node TEXT="true"/></node></node><node TEXT="password"><node TEXT="type"><node TEXT="string"/></node><node TEXT="required"><node TEXT="true"/></node></node></node><node TEXT="cat"><node TEXT="id"><node TEXT="type"><node TEXT="integer"/></node><node TEXT="incremental"><node TEXT="true"/></node></node><node TEXT="canMeow"><node TEXT="true"/></node></node></node><node TEXT="abilities"/></node>
-</map>
